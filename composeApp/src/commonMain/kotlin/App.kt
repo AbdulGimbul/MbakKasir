7aef692e1dfb680aa104787745bd7af9de2e261e
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2024-12-21 18:48:27 +0700

    Upgrade kotlin version and prepare setup for in case need some permission

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -16,9 +16,63 @@ fun App() {
     ) {
         val navController = rememberNavController()
         val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+
+        // For in case need some permission, full guide in stevdza-san moko-permission video
+        /*
+        val snackBarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+        val factory = rememberPermissionsControllerFactory()
+        val controller = remember(factory) {
+            factory.createPermissionsController()
+        }
+        BindEffect(controller)
+         */
+
         SetupNavHost(
             navController = navController,
             windowSize = windowSizeClass.windowWidthSizeClass
         )
     }
 }
+
+// For in case need some permission, full guide in stevdza-san moko-permission video
+/*
+suspend fun checkPermission(
+    permission: Permission,
+    controller: PermissionsController,
+    snackbarHostState: SnackbarHostState
+){
+    val granted = controller.isPermissionGranted(permission)
+    if (!granted){
+        try {
+            controller.providePermission(permission)
+        } catch (e: DeniedException){
+            val result = snackbarHostState.showSnackbar(
+                message = "Denied",
+                actionLabel = "Open Settings",
+                duration = SnackbarDuration.Short
+            )
+            if (result == SnackbarResult.ActionPerformed){
+                controller.openAppSettings()
+            }
+        } catch (e: DeniedAlwaysException){
+            val result = snackbarHostState.showSnackbar(
+                message = "Permanently denied",
+                actionLabel = "Open Settings",
+                duration = SnackbarDuration.Short
+            )
+            if (result == SnackbarResult.ActionPerformed){
+                controller.openAppSettings()
+            }
+        } catch (e: RequestCanceledException){
+            snackbarHostState.showSnackbar(
+                message = "Request canceled."
+            )
+        }
+    } else {
+        snackbarHostState.showSnackbar(
+            message = "Permission already granted."
+        )
+    }
+}
+*/
